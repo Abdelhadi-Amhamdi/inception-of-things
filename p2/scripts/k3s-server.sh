a397b73f62731d8d3c97a90c899e4c4def13eeb2
Abdelhadi Amhamdi <aamhamdi943@gmail.com>
2026-05-11 15:16:17 +0000

    clean and fix requirments part 2

--- p2/scripts/k3s-server.sh
+++ p2/scripts/k3s-server.sh
@@ -1,19 +1,21 @@
 #!/bin/sh
 set -e
 
+apt update && apt install -y curl
+
 echo "[INFO] Installing K3s (server mode)..."
 
-if [ -f /usr/local/bin/k3s-uninstall.sh ]; then
-  echo "[INFO] Removing previous K3s install..."
-  /usr/local/bin/k3s-uninstall.sh
+if [ -f /usr/local/bin/k3s ]; then
+  echo "[INFO] K3s is already installed. Skipping installation."
+else
+  echo "[INFO] K3s is not installed. Proceeding with installation."
+  curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION="v1.28.15+k3s1" INSTALL_K3S_EXEC="server" sh -s - \
+    --token 12345 \
+    --write-kubeconfig-mode 644 \
+    --bind-address 192.168.56.110 \
+    --advertise-address 192.168.56.110 \
+    --node-ip 192.168.56.110
 fi
-
-curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION="v1.28.15+k3s1" INSTALL_K3S_EXEC="server" sh -s - \
-  --token 12345 \
-  --write-kubeconfig-mode 644 \
-  --bind-address 192.168.56.110 \
-  --advertise-address 192.168.56.110 \
-  --node-ip 192.168.56.110
 
 while [ ! -f /etc/rancher/k3s/k3s.yaml ]; do
     echo "Waiting for K3s to be ready..."
